bddc13355615bc1376840c6ebc7a6dfb11c9dd6c
Luiz Gabriel <luiz.gabriel.cristo@hotmail.com>
2016-01-20 17:51:29 -0200

    Initial Commit

---
+++ ProtentionBuilder/Plugin.cs
@@ -0,0 +1,40 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+using System.Windows.Forms.Integration;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.Windows;
+
+namespace Urbbox.AutoCAD.ProtentionBuilder
+{
+    public class Plugin
+    {
+        private static PaletteSet _mainPallet;
+
+        [CommandMethod("URBPROTENSION")]
+        public static void ShowPallet()
+        {
+            if (_mainPallet != null) return;
+            _mainPallet = InitializeMainPallet();
+                
+            var especificationsControl = new ElementHost
+            {
+                AutoSize = true,
+                Dock = DockStyle.Fill,
+                Child = new EspecificationsControl()
+            };
+
+            _mainPallet.Add("Especificações", especificationsControl);
+        }
+
+        private static PaletteSet InitializeMainPallet()
+        {
+            return new PaletteSet("Urbbox | Construtor de Lajes")
+            {
+                Size = new Size(300, 600),
+                DockEnabled = (DockSides) ((int) DockSides.Left + (int) DockSides.Right),
+                Visible = true,
+                KeepFocus = true
+            };
+        }
+    }
+}
